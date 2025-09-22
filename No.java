8c1223b85e21a798e03a9088564425f4f6313153
Gustavo Kiyoshi Ikeda <gustavokikeda@gmail.com>
2025-09-21 21:05:51 -0300

    Add files via upload

---
+++ No.java
@@ -0,0 +1,15 @@
+public class No {
+    String nome;
+    No esquerda;
+    No direita;
+
+    public No(String nome) {
+        this.nome = nome;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ Raizes.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class Raizes {
+    ArrayList<No> raizes;
+    int quantidade_raizes;
+
+    public Raizes(){
+        this.raizes = new ArrayList<>();
+        this.quantidade_raizes = 0;
+    }
+
+    public int getQuantidade_raizes(){
+        return quantidade_raizes;
+    }
+
+    public void inserir_raiz(No nova_raiz){
+        raizes.add(nova_raiz);
+        this.quantidade_raizes++;
+    }
+
+    public void remover_raiz(No raiz_remover){
+        raizes.remove(raiz_remover);
+        this.quantidade_raizes--;
+    }
+
+    public No get_No(No target_no){
+        for(No raiz_atual : raizes)
+        {
+            No resultado = percorrer(raiz_atual, target_no);
+            if(resultado != null)
+            {
+                return resultado;
+            }
+        }
+        return null;
+    }
+
+    public No percorrer(No raiz_atual, No target_no){
+        if(raiz_atual == null)
+        {
+            return null;
+        }
+        if(raiz_atual.nome.equals(target_no.nome))
+        {
+            return raiz_atual;
+        }
+        No esquerda = percorrer(raiz_atual.esquerda, target_no);
+        if(esquerda != null)
+        {
+            return esquerda;
+        }
+        return percorrer(raiz_atual.direita, target_no);
+    }
+
+}
